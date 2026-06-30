07fca25f4024e4bcd5f71dba1889885d04c28672
Loading404-ux <subho468bhat@gmail.com>
2026-07-01 02:10:08 +0530

    Day-14-Backlog-if

---
+++ DAY-14-JS/7_if-Statement/app.js
@@ -0,0 +1,19 @@
+// if-Statement.
+console.log("Code Before if-statement.");
+let age = 23;
+if (age >= 18) {
+    console.log("You can VOTE!!!");
+    console.log("You can also Drive!!");
+}
+if (age < 18) {
+    console.log("You cannot VOTE!!!");
+    console.log("You cannot also Drive!!");
+}
+console.log("Code After if-statement.");
+
+
+
+let firstName = "Subho";
+if (firstName == "Subho") {
+    console.log(`Welcome ${firstName}`);
+}
