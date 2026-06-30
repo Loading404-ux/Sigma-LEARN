e48f3a173928b635216bccfd41f716ada489684f
Loading404-ux <subho468bhat@gmail.com>
2026-07-01 02:01:35 +0530

    Day-14-Backlog-Comparisons

---
+++ DAY-14-JS/5_Comparison_Operators/app.js
@@ -0,0 +1,25 @@
+// Comparsion Operators
+
+let age=18;
+
+console.log(`This age Condition is :${age>=18}`);
+
+console.log(5==5);
+console.log(4!=5);
+
+console.log(5!=5);
+console.log(4==7);
+
+// But JS matches the value on comparison, not the data type.
+
+console.log(5=='5');
+
+let num=5;
+let str='5';
+console.log(num==str);
+console.log(typeof(num));
+console.log(typeof(str));
+
+// === compares value & type
+
+console.log(5==='5');
---
+++ DAY-14-JS/6_Comparison_for_non-numbers/app.js
@@ -0,0 +1,5 @@
+// Comparison for NON-numbers
+
+console.log('a'<'b');
+console.log('A'>'b');
+console.log('*'<'&');
