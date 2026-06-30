a352adc1663758de0d098affcf8fb2c7200ca8ac
Loading404-ux <subho468bhat@gmail.com>
2026-07-01 00:38:08 +0530

    Day-14-Backlog-Template_Literals

---
+++ DAY-14-JS/3_Template_Literals/app.js
@@ -0,0 +1,13 @@
+let a=5;
+let b=17;
+
+// normal console.log
+// console.log("The total amount is :", a + b ,"Rupees.");
+
+// using String variable.
+// let output="The total amount is : "+ (a + b) +" Rupees.";
+// console.log(output);
+
+// Template Literals
+// let output=`The total amount is: ${a+b} Rupees.`;
+console.log(`The total amount is: ${a+b} Rupees.`);
