91e14333d376637821e70bc6eb8b13279e552ef4
Loading404-ux <subho468bhat@gmail.com>
2026-07-01 01:14:14 +0530

    Day-14-Backlog-Operators

---
+++ DAY-14-JS/4_Operators_JS/app.js
@@ -0,0 +1,17 @@
+// Arithmetic Operators
+let a=10;
+let b=5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+
+// Unary Operators
+console.log(a++); //postfix
+console.log(++a); //prefix
+
+// Assignment Operators
+b=a;
+console.log(b);
