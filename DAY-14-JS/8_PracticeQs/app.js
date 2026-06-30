dd14ac06b29cc9aaada1b1a9b05b7a391072b3a0
Loading404-ux <subho468bhat@gmail.com>
2026-07-01 02:20:41 +0530

    Day-14-Backlog-PracticeQs

---
+++ DAY-14-JS/8_PracticeQs/app.js
@@ -0,0 +1,13 @@
+// Traffic Light.
+
+let trafficLightColor="green";
+
+if(trafficLightColor==="red"){
+    console.log(`STOP!!! . Traffic light color is ${trafficLightColor}`);
+}
+if(trafficLightColor==="yellow"){
+    console.log(`GO SLOW!! Traffic light color is ${trafficLightColor}`);
+}
+if(trafficLightColor==="green"){
+    console.log(`GO!! Traffic light color is ${trafficLightColor}`);
+}
