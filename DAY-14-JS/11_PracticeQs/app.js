ae58a544c3706440bb0ba701213f9f955b325454
Loading404-ux <subho468bhat@gmail.com>
2026-07-02 00:38:40 +0530

    PracticeQs

---
+++ DAY-14-JS/11_PracticeQs/app.js
@@ -0,0 +1,14 @@
+// PracticeQs
+// Calculate POPcorn price.
+
+let popCornSize = "S";
+
+if(popCornSize === "XL"){
+    console.log(`Popcorn ${popCornSize} size : Rs. 250.`);
+}else if(popCornSize === "L"){
+    console.log(`Popcorn ${popCornSize} size : Rs. 200.`);
+}else if(popCornSize === "M"){
+    console.log(`Popcorn ${popCornSize} size : Rs. 100.`);
+}else{
+    console.log(`Popcorn ${popCornSize} size : Rs. 50.`);
+}
