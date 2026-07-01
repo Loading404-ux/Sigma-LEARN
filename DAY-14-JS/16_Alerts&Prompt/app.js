a6a92bed8bc2729927a447f8fe9529abd38b9a8d
Loading404-ux <subho468bhat@gmail.com>
2026-07-02 02:16:22 +0530

    DAY-14-JS-Backlog-Alerts&Prompt

---
+++ DAY-14-JS/16_Alerts&Prompt/app.js
@@ -0,0 +1,25 @@
+// simple alert
+// alert("something is wrong!!!");
+
+
+// console /.log /.error /.warn
+// console.log("This is normal log!!");
+// console.error("This the error log!!");
+// console.log("This is a warning log!!");
+
+
+// simple prompt.
+// prompt("Enter the password.");
+
+// prompt data store in variable.
+// let name=prompt("Enter your name:");
+// console.log(name);
+
+
+// using all of this
+let firstName = prompt("Enter your first name :");
+let lastName = prompt("Enter your last name : ");
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+let msg="Welcome!! , "+ fullName;
+alert(msg);
